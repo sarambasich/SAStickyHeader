914a68b6e93178ae72fb3a3bab40a4fa2c34fcf8
Stefan Arambasich <ArtisOracle@gmail.com>
2016-12-29 21:17:46 -0500

    better keyboard handling

--- SAStickyHeader/SAStickyHeaderViewController.swift
+++ SAStickyHeader/SAStickyHeaderViewController.swift
@@ -48,13 +48,18 @@ open class SAStickyHeaderViewController: UIViewController, UIScrollViewDelegate 
         super.viewDidLoad()
         
         configureViews()
-        configureKeyboardSupport()
     }
 
     open override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
 
         stopKeyboardSupport()
+    }
+
+    open override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        configureKeyboardSupport()
     }
 
     /**
